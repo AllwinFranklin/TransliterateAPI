febf9d0b2361d5fa5d69357779a158270562c73c
AllwinFranklin <allwinanto360@gmail.com>
2022-10-04 12:46:11 +0100

    Add project files.

---
+++ TransliterateAPI/Controllers/TranslitController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransliterateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TranslitController : ControllerBase
+    {
+        // GET: api/<TranslitController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TranslitController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TranslitController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TranslitController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TranslitController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
